ceebd0ab0d48a78f2e58bd2ffaca1669f2e099df
TechieTemi <apparelreepublik@gmail.com>
2023-08-18 15:59:54 +0100

    object-oriented programming with Python

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+str = str[39:67] + str[-22:-17] + str[:6]
+print(str)
