4adc688395508ea014d72afaf1b03b158df9a037
TechieTemi <apparelreepublik@gmail.com>
2023-10-04 00:11:37 +0100

    print alphabet

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -2,4 +2,4 @@
 for alphabet in range(97, 123):
     if chr(alphabet) == 'e' or chr(alphabet)== 'q':
         continue
-    print("{}".format(chr(alphabet)), end='')	
+    print("{}". format(chr(alphabet)), end='')	
