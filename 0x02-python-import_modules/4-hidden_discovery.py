ce17ac19a30f6b75e07eeeab016e0df9346e2355
TechieTemi <apparelreepublik@gmail.com>
2023-10-06 00:05:50 +0100

    0x02-python-import_modules

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
-import hidden_4
-if __name__ == "__main__":
-    for mod in dir(hidden_4):
-        if not mod.startswith("__"):
-            print(mod)
 
+if __name__ == "__main__":
+    """Print all names defined by hidden_4 module."""
+    import hidden_4
+
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
