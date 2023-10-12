3964febaeef01b4230586156404e6ce3a61b30cb
TechieTemi <apparelreepublik@gmail.com>
2023-10-12 17:58:13 +0100

    3-tasks

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,14 +2,11 @@
 def square_matrix_simple(matrix=[]):
     len_row = len(matrix)
     len_column = len(matrix[0])
-
     square_matrix = []
-
     for i in range(len_row):
         row = []
         for j in range(len_column):
             row.append(matrix[i][j] ** 2)
         square_matrix.append(row)
-
     return square_matrix
 
