e060ec9648242b143cdec02b23563027cf083b73
TechieTemi <apparelreepublik@gmail.com>
2024-01-12 10:11:57 -0800

    0-add_integer

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -8,14 +8,9 @@ An integer argument or float argument can also be passed to it."""
 
 
 def add_integer(a, b=98):
-    """Return the sum of two integers or floats as integers
-
-    Args:
-        a: first argument
-        b: second argument
-    Returns:
-        Sum of the two arguments
     """
+    Returns:
+        Sum of the two arguments """
     if ((not isinstance(a, int) and not isinstance(a, float))):
         raise TypeError("a must be an integer")
     if ((not isinstance(b, int) and not isinstance(b, float))):
